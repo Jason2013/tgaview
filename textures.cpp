d3e36ca2a5927b5914cea2e1728d6a8e25d8104b
Jason Chen <shchenchang@gmail.com>
2022-12-03 20:35:35 +0800

    Add `GAImage` usage code.

--- textures.cpp
+++ textures.cpp
@@ -17,6 +17,19 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+#include "tgaimage.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
+int main1(int argc, char** argv) {
+    TGAImage image(100, 100, TGAImage::RGB);
+    image.set(52, 41, red);
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    return 0;
+}
+
 int main()
 {
     // glfw: initialize and configure
