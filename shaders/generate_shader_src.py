95cb2d156b24d6e3ed27f3c87555be3a0579a764
Jason Chen <shchenchang@gmail.com>
2022-12-04 17:07:28 +0800

    Code clean.

--- shaders/generate_shader_src.py
+++ shaders/generate_shader_src.py
@@ -3,26 +3,8 @@
 import os
 import sys
 
-def convert_old(in_file, out_file):
-    print(in_file)
-    print(out_file)
-    with open(out_file, "w", encoding="utf-8") as out_file, open(in_file, "rb") as in_file:
-        data = in_file.read()
-        n = 0
-        for c in data:
-            out_file.write("0x{:02x},".format(c))
-            n += 1
-            if n == 16:
-                out_file.write("\n")
-                n = 0
-            else:
-                out_file.write(" ")
-        out_file.write("0x00\n")
 
 def convert(in_file, out_file):
-    # print(in_file)
-    # print(out_file)
-    # with open(out_file, "w", encoding="utf-8") as out_file, open(in_file, "rb") as in_file:
         data = in_file.read()
         n = 0
         for c in data:
@@ -37,6 +19,7 @@ def convert(in_file, out_file):
                 out_file.write(" ")
         out_file.write("0x00\n")
 
+
 if __name__ == "__main__":
     print(sys.argv)
     input_dir = os.path.join(sys.argv[1], "shaders")
@@ -50,22 +33,18 @@ if __name__ == "__main__":
         hf.write("#ifndef %s\n" % output_header_macro)
         hf.write("#define %s\n\n" % output_header_macro)
         sf.write("#include \"%s\"\n" % (output_file_basename + ".h"))
+
         for i in range(4, len(sys.argv)):
             shader_file = sys.argv[i]
             input_shader_file = os.path.join(input_dir, shader_file)
             shader_var = shader_file.replace(".", "_")
+
             hf.write("extern const char %s_code[];\n" % shader_var)
             sf.write("\n// This variable '%s_code' is generate from file: '%s'\n" % (shader_var, os.path.normpath(input_shader_file)))
             sf.write("const char %s_code[] = {\n" % shader_var)
+
             with open(input_shader_file, "rb") as isf: 
                 convert(isf, sf)
             sf.write("};\n")
+
         hf.write("\n#endif // %s\n" % output_header_macro)
-
-                # hf.write hs
-                # sf
-# k
-
-
-    # convert(os.path.join(sys.argv[1], "shaders", "4.1.texture.fs"), os.path.join(sys.argv[2], "4.1.texture.fs.h"))
-    # convert(os.path.join(sys.argv[1], "shaders", "4.1.texture.vs"), os.path.join(sys.argv[2], "4.1.texture.vs.h"))
