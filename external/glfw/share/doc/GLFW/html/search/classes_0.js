68b5e90a8a4166437a9ee31d56048e80e98e8496
Jason Chen <shchenchang@gmail.com>
2022-12-04 11:37:58 +0800

    Add `glfw` external package.
    `glfw` build commands:
    
    ```
    cd ${GLFW}
    cmake -S . -B build --install-prefix=$(TGA_VIEWER}/external
    cmake --build build --config Release
    cmake --install build --config Release
    ```

---
+++ external/glfw/share/doc/GLFW/html/search/classes_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['glfwallocator_574',['GLFWallocator',['../struct_g_l_f_wallocator.html',1,'']]],
+  ['glfwgamepadstate_575',['GLFWgamepadstate',['../struct_g_l_f_wgamepadstate.html',1,'']]],
+  ['glfwgammaramp_576',['GLFWgammaramp',['../struct_g_l_f_wgammaramp.html',1,'']]],
+  ['glfwimage_577',['GLFWimage',['../struct_g_l_f_wimage.html',1,'']]],
+  ['glfwvidmode_578',['GLFWvidmode',['../struct_g_l_f_wvidmode.html',1,'']]]
+];
