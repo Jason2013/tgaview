e5d0cf29fb09f2c2d22bab837feb5f06e0c040f9
Chang Chen <shchenchang@gmail.com>
2022-12-06 11:03:37 +0800

    Flip image vertically.

--- src/textures.cpp
+++ src/textures.cpp
@@ -124,10 +124,10 @@ int main(int argc, char* argv[])
     // ------------------------------------------------------------------
     float vertices[] = {
         // positions         // texture coords
-         1.0f,  1.0f, 0.0f,  1.0f, 1.0f, // top right
-         1.0f, -1.0f, 0.0f,  1.0f, 0.0f, // bottom right
-        -1.0f, -1.0f, 0.0f,  0.0f, 0.0f, // bottom left
-        -1.0f,  1.0f, 0.0f,  0.0f, 1.0f  // top left 
+         1.0f,  1.0f, 0.0f,  1.0f, 0.0f, // top right
+         1.0f, -1.0f, 0.0f,  1.0f, 1.0f, // bottom right
+        -1.0f, -1.0f, 0.0f,  0.0f, 1.0f, // bottom left
+        -1.0f,  1.0f, 0.0f,  0.0f, 0.0f  // top left 
     };
     unsigned int indices[] = {  
         0, 1, 3, // first triangle
